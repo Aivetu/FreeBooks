8fea9e1330a6598f715feaad8a0117ac3e6b956b
Aivetu <habibatoiza2@gmail.com>
2022-06-20 09:33:37 +0100

    changed directory

--- src/main/java/com/example/freebooks/BooksRepo.java
+++ src/main/java/com/example/freebooks/BooksRepo.java
@@ -2,7 +2,6 @@ package com.example.freebooks;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
